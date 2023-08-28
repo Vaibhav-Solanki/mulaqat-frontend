4315bc718c046fb6be45d9c0d23f506fa5f22a65
tirthesh-bohra <tirthesh.bohra@gmail.com>
2023-08-28 10:42:59 +0530

    v2
    @Vaibhav-Solanki do check this code got it from some public repo, looks promising hoping to use this UI and build on it, there are some bugs, updated the same for backend as well, seems like backend isn't required that much.

--- src/routes/AuthProvider.js
+++
@@ -1,7 +0,0 @@
-import Login from './Login';
-
-export default function AuthProvider (routes,isLoggedIn){
-  return routes.map(route=> ({
-    ...route, element: route.isProtected && !isLoggedIn ? <Login /> : route.element
-  }) )
-};
